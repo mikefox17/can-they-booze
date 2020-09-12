43731ee6e59bbaeddb9b6f3b34a9c7b7806e51a5
Michael Fox <mikefox17@gmail.com>
2020-09-12 01:06:42 -0400

    reworked logic, removed moment.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 let button = document.querySelector("#submit");
 let input = document.querySelector("#input");
+let date = new Date().toJSON().slice(0, 10).replace(/-/g, "");
+let formatDate = parseInt(date, 10);
+console.log(formatDate);
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
-  let age = moment(input.value.toString(), "MMDDYYYY").fromNow();
-  let formatAge = parseInt(age, 10);
-
-  if (formatAge > 21) {
+  let inputDob = input.value;
+  let dob = parseInt(inputDob, 10);
+  console.log(dob);
+  let age = Math.floor((formatDate - dob) / 10000);
+  if (age >= 21) {
     document.querySelector(
       ".display"
-    ).innerHTML = `<h2 class="text-center">YOU CAN SERVE THEM. THEY ARE <strong class="text-success">${
-      formatAge - 1
-    }</strong> YEARS OLD</h2></>`;
+    ).innerHTML = `<h2 class="text-center">YOU CAN SERVE THEM. THEY ARE <strong class="text-success">${age}</strong> YEARS OLD</h2></>`;
   } else {
     document.querySelector(
       ".display"
-    ).innerHTML = `<h2 class="text-center">DO NOT SERVE THEM. THEY ARE <strong class="text-danger">${
-      formatAge - 1
-    }</strong> YEARS OLD</h2>`;
+    ).innerHTML = `<h2 class="text-center">DO NOT SERVE THEM. THEY ARE <strong class="text-danger">${age}</strong> YEARS OLD</h2>`;
   }
   input.value = "";
 });
